26282394ead3519125c5121638516a39b20a2d0f
suzhanlee <wlscww@kakao.com>
2025-06-22 16:24:53 +0900

    settings: 초기 프로젝트 세팅

---
+++ src/main/java/com/tutor_link/tutor_link/TutorLinkApplication.java
@@ -0,0 +1,13 @@
+package com.tutor_link.tutor_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutor_link/tutor_link/TutorLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutor_link.tutor_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
